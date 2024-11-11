2d26932a49380ed7dfa8ecb561485ea1f4ece7b7
madiharuhe <154939368+madiharuhe@users.noreply.github.com>
2024-11-11 12:30:47 +0530

    Update p5.c

--- p5.c
+++ p5.c
@@ -1,23 +1,55 @@
 #include<stdio.h>
-
-int a[10], n, i;
-
-void s(int *x, int *y) { int t = *x; *x = *y; *y = t; }
-
-void Heap(int n) {
-    for (int ch = 1, ps; ch <= n; ch++) {
-        for (int t = a[ch]; ch > 1 && t > a[ps = ch / 2]; ch = ps) a[ch] = a[ps];
-        a[ch] = t;
-    }
+int swap(int *x, int *y);
+int heap(int n);
+int heapsort (int n);
+int a[10];
+int main()
+{
+int i,n;
+printf("Enter the limit\n");
+scanf("%d",&n);
+printf("Enter the elements \n");
+for (i=1;i<=n;i++)
+scanf("%d",&a[i]);
+heap(n);
+heapsort(n);
+printf("Sorted elements \n");
+for (i=1;i<=n;i++)
+printf("%d\t",a[i]);
 }
-
-void Heapsort(int n) {
-    while (n > 1) s(&a[1], &a[n--]), Heap(n);
+int heap(int n)
+{
+int ch, ps, temp;
+for (ch=1;ch<=n;ch++)
+{
+temp=a[ch];
+ps=ch/2;
+while (ch>1 && temp > a[ps])
+{
+a[ch] = a[ps];
+ch=ps;
+ps=ch/2;
+if (ps< 1)
+ps=1;
 }
-
-int main() {
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++) scanf("%d", &a[i]);
-    Heap(n); Heapsort(n);
-    for (i = 1; i <= n; i++) printf("%d ", a[i]);
+a[ch] = temp;
+}
+}
+int heapsort (int n)
+{
+    while (n>1)
+{
+swap(&a[1],&a[n]);
+{
+n--;
+heap(n);
+}
+}
+}
+int swap(int *x, int *y)
+{
+int temp;
+temp=*x;
+*x = *y;
+*y =temp;
 }
