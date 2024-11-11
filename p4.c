022ff705f32a4a4d7504a68691c7ebc26ac200b4
madiharuhe <154939368+madiharuhe@users.noreply.github.com>
2024-11-11 12:28:16 +0530

    Update p4.c

--- p4.c
+++ p4.c
@@ -1,21 +1,29 @@
-
 #include<stdio.h>
 #include<time.h>
-
-int a[10], n, i, j, v;
-clock_t s, e;
-
-int main() {
-    scanf("%d", &n);
-    s = clock();
-    for (i = 0; i < n; i++) scanf("%d", &a[i]);
-    for (i = 1; i < n; i++) {
-        v = a[i];
-        for (j = i - 1; j >= 0 && a[j] > v; j--) a[j + 1] = a[j];
-        a[j + 1] = v;
-    }
-    e = clock();
-    for (i = 0; i < n; i++) printf("%d ", a[i]);
-    printf("\nTime = %f\n", (double)(e - s) / CLOCKS_PER_SEC);
-    return 0;
+void main()
+{
+int a[10],v,j,n,i;
+clock_tstart,end;
+printf("\n Enter the order of an array\n");
+scanf("%d",&n);
+start=clock();
+printf("Enter the elements of an array\n");
+for (i=1;i<=n;i++)
+scanf("%d",&a[i]);
+for (i=1;i<=n;i++)
+{
+v=a[i];
+j=i-1;
+while (j >= 00 && a[j] > v)
+{
+a[j+1]=a[j];
+j=j-1;
+}
+a[j+1]=v;
+}
+end = clock();
+printf("\n The Sorted array is \n");
+for (i=1;i<=n;i++)
+printf("\n %d",a[i]);
+printf("\nTime = %f",(double)(end-start)/CLOCKS_PER_SEC);
 }
