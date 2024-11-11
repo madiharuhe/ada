7649af31d09190b80704b9b28edbaf0741a6b8fe
madiharuhe <154939368+madiharuhe@users.noreply.github.com>
2024-11-11 12:25:27 +0530

    Update p3.c

--- p3.c
+++ p3.c
@@ -1,27 +1,40 @@
-#include <stdio.h>
-#include <time.h>
-
-void merge_sort(int a[], int l, int h) {
-    if (l < h) {
-        int m = (l + h) / 2, i = l, j = m + 1, k = l, b[20];
-        merge_sort(a, l, m);
-        merge_sort(a, m + 1, h);
-        while (i <= m && j <= h) b[k++] = (a[i] < a[j]) ? a[i++] : a[j++];
-        while (i <= m) b[k++] = a[i++];
-        while (j <= h) b[k++] = a[j++];
-        for (i = l; i <= h; i++) a[i] = b[i];
-    }
+#include<stdio.h>
+#include<time.h>
+int selection_sort(int a[], int n);
+int main()
+{
+int i,n,a[20],key;
+clock_t end,start;
+printf("Enter the size of an array\n");
+scanf("%d",&n);
+printf("Enter the array elements\n");
+for (i=0;i<n;i++)
+scanf("%d",&a[i]);
+start = clock();
+selection_sort(a,n);
+end = clock();
+printf("Sorted elements are\n");
+for (i=0;i<n;i++)
+printf("\n %d",a[i]);
+printf("\nTime = %f",(double)(end - start)/CLOCKS_PER_SEC);
 }
-
-int main() {
-    int n, a[20];
-    clock_t start = clock();
-    printf("Enter size and elements: ");
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-    merge_sort(a, 0, n - 1);
-    printf("Sorted elements:\n");
-    for (int i = 0; i < n; i++) printf("%d ", a[i]);
-    printf("\nTime = %f\n", (double)(clock() - start) / CLOCKS_PER_SEC);
-    return 0;
+int selection_sort(int a[], int n)
+{
+int i,j,pos,small,temp;
+for(i=0;i<n-1;i++)
+{
+small = a[i];
+pos = i;
+for (j=i+1;j<n;j++)
+{
+if (a[j] < small)
+{
+small = a[j];
+pos=j;
+}
+}
+temp = a[pos];
+a[pos] = a[i];
+a[i] = temp
+}
 }
