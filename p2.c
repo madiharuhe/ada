232c5eda3849ce1bad1615b703754f76a1f784d0
madiharuhe <154939368+madiharuhe@users.noreply.github.com>
2024-11-11 12:20:57 +0530

    Update p2.c

--- p2.c
+++ p2.c
@@ -1,26 +1,51 @@
-#include <stdio.h>
-#include <time.h>
-
-void selection_sort(int a[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int pos = i;
-        for (int j = i + 1; j < n; j++)
-            if (a[j] < a[pos]) pos = j;
-        int temp = a[pos];
-        a[pos] = a[i];
-        a[i] = temp;
-    }
+#include<stdio.h>
+#include<time.h>
+int mergesort(int *, int , int);
+int merge(int *, int, int, int);
+void main ()
+{
+int i,n,a[20];
+clock_tstart,end;
+start = clock();
+printf("Enter the limit \n");
+scanf("%d",&n);
+printf("Enter the elements \n");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+mergesort(a,0,n-1);
+end = clock();
+printf("The sorted elements are\n");
+for (i=0;i<n;i++)
+printf("%d\n",a[i]);
+printf("\n Time = %f",(double)(end - start)/CLOCKS_PER_SEC);
+return;
 }
-
-int main() {
-    int n, a[20];
-    clock_t start = clock();
-    printf("Enter size and elements: ");
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-    selection_sort(a, n);
-    printf("Sorted elements:\n");
-    for (int i = 0; i < n; i++) printf("%d ", a[i]);
-    printf("\nTime = %f\n", (double)(clock() - start) / CLOCKS_PER_SEC);
-    return 0;
+int mergesort (int a[], int low, int high)
+{
+int mid;
+if (low < high)
+{
+mid = (low + high)/2;
+mergesort(a,low,mid);
+mergesort(a,mid+1,high);
+merge(a,low,mid,high);
+}
+}
+int merge(int a[], int low,int mid, int high)
+{
+int i,j,k,h,b[20];
+h=i=low;
+j=mid + 1;
+while (h <= mid && j<=high)
+    b[i++] = a[h++];
+else
+b[i++]=a[j++];
+if (h > mid)
+for(k=j;k<=high;k++)
+b[i++] = a[k];
+else
+for (k=h;k<=mid;k++)
+b[i++] = a[k];
+for(k=low;k<=high;k++)
+a[k] = b[k];
 }
